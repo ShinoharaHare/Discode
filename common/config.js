424af115810b088a9f855a6acb4207134fca6265
ShinoharaHare <anna.shike1822@gmail.com>
2019-12-31 20:52:14 +0800

    Update config.js

--- common/config.js
+++ common/config.js
@@ -1,9 +1,9 @@
 const process = require('process');
 
-console.log(process.env.MONGOLAB_URI);
+console.log(process.env.MONGODB_URI)
 
 const config = {
-    mongodbUrl: 'mongodb://localhost:27017/discode' || process.env.MONGOLAB_URI,
+    mongodbUrl: process.env.MONGODB_URI || 'mongodb://localhost:27017/discode',
     port: 80,
     secret: '5ff567bdd7dfae2792c8ad5d5f10d6418804dfbb8ff8b03ebcdd2b7381e9bc7f',
 };
