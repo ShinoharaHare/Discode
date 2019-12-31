0e81d25cc9f2120a48fbea95d36c23b8c9b83c92
ShinoharaHare <anna.shike1822@gmail.com>
2019-12-31 23:46:18 +0800

    .

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/api', require('./routes/api'));
 app.use(history());
 
 // 靜態檔案
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Discode-Front-End/dist')));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
